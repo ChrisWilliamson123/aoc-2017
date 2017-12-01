10434b0e007d9c128ad4bb9a5a86145b555477a4
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2017-12-01 11:17:09 +0000

    More refactors

--- day1/main.py
+++ day1/main.py
@@ -1,13 +1,18 @@
+def get_next_index(number_list, current_index, step):
+    nextIndex = current_index + step
+
+    if nextIndex > len(number_list)-1:
+        return nextIndex - len(number_list)
+
+    return nextIndex
+
 def main():
     inputString = open('input.txt', 'r').read()
     step = len(inputString) / 2
 
     total = 0
     for i in range(0, len(inputString)):
-        nextIndex = i + step
-
-        if nextIndex > len(inputString)-1:
-            nextIndex = nextIndex - len(inputString)
+        nextIndex = get_next_index(inputString, i, step)
 
         if inputString[i] == inputString[nextIndex]:
             total += int(inputString[i])
