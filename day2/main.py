ff4725db8f4abf8938d9225bece657922deb67a8
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2017-12-02 12:14:03 +0000

    Added day 2 pt 1

---
+++ day2/main.py
@@ -0,0 +1,10 @@
+def main():
+    sheet = [line.rsplit('\t') for line in open('input.txt', 'r').read().splitlines()]
+    checksum = 0
+    for row in sheet:
+        row = [int(x) for x in row]
+        checksum = checksum + (max(row) - min(row))
+    print(checksum)
+
+if __name__ == "__main__":
+    main()
