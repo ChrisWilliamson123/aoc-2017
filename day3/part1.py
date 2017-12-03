523576a70dcad944ecf31b27806990e45a3cabe1
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2017-12-03 17:11:07 +0000

    Done part 1

---
+++ day3/part1.py
@@ -0,0 +1,37 @@
+def get_ring_index(input_number):
+    found_ring = False
+    ring_index = 1
+    start = 1
+
+    while not found_ring:
+        end = start + (ring_index * 8) - 1
+        
+        if start <= input_number <= end:
+            found_ring = True
+        else:
+            ring_index += 1
+            start = end + 1
+
+    return (ring_index, start)
+
+def get_anchor_digits(start, ring_index):
+    right = start + (ring_index - 1)
+    top = right + (2*ring_index)
+    left = top + (2*ring_index)
+    bottom = left + (2*ring_index)
+
+    return [right, top, left, bottom]
+
+def main():
+    input_number = 289325
+
+    ring_index, start = get_ring_index(input_number)
+    anchors = get_anchor_digits(start, ring_index)
+    diffs = map(lambda x: abs(x - input_number), anchors)
+    manhattan_distance = min(diffs) + ring_index
+
+    print(manhattan_distance)
+
+
+if __name__ == '__main__':
+    main()
