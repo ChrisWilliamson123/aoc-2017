cdabdf2c7409a8d6c95d55ee37c718d494e38e26
Chris Williamson <chris.williamson@bbc.co.uk>
2017-12-05 15:23:18 +0000

    Converted indentation to spaces.

--- day5/main.py
+++ day5/main.py
@@ -1,16 +1,16 @@
 def main():
-	part_two = True
-	instructions = [int(line.rstrip()) for line in open('input.txt', 'r').readlines()]
-	i = steps = 0
+    part_two = True
+    instructions = [int(line.rstrip()) for line in open('input.txt', 'r').readlines()]
+    i = steps = 0
 
-	while i < len(instructions):
-		old = i
-		i += instructions[i]
-		change = -1 if part_two and instructions[old] >= 3 else 1
-		instructions[old] += change
-		steps += 1
+    while i < len(instructions):
+        old = i
+        i += instructions[i]
+        change = -1 if part_two and instructions[old] >= 3 else 1
+        instructions[old] += change
+        steps += 1
 
-	print(steps)
+    print(steps)
 
 if __name__ == '__main__':
     main()
