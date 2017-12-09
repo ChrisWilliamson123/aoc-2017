63a2b692532ee95612b42d4dd27491c42f721a90
Chris Williamson <chris.williamson@bbc.co.uk>
2017-12-09 10:49:08 +0000

    Refactored.

--- day9/main.py
+++ day9/main.py
@@ -1,38 +1,35 @@
 import re
 
 def cancel_character(stream, index):
-    index -= 1
     cancel_count = 0
-    prev_char = stream[index]
+    prev_char = stream[index-1]
     while prev_char is '!':
         cancel_count += 1
         index -= 1
-        prev_char = stream[index]
+        prev_char = stream[index-1]
     return False if cancel_count % 2 is 0 else True
 
 def main():
     stream = open('input.txt', 'r').read()
-    depth = 0
-    score = 0
+    depth = score = garbage_char_count = 0
     in_garbage = False
-    garbage_char_count = 0
 
     for i in range(0, len(stream)):
         if not cancel_character(stream, i):
             char = stream[i]
-            if not in_garbage:
+
+            if in_garbage and char not in ['!', '>']:
+                garbage_char_count += 1
+            elif not in_garbage:
                 if char is '{':
                     depth += 1
-                if char is '}' and depth > 0:
+                elif char is '}' and depth > 0:
                     score += depth
                     depth -= 1
-
-            elif not cancel_character(stream, i) and char not in ['!', '>']:
-                garbage_char_count += 1
                 
             if char is '<':
                 in_garbage = True
-            if char is '>':
+            elif char is '>':
                 in_garbage = False
       
     print(score, garbage_char_count)
