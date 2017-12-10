d6af1bb4521921e1f595a8ef009d63b97ce08a40
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2017-12-10 21:45:48 +0000

    Done part 1

--- day10/main.py
+++ day10/main.py
@@ -1,25 +1,24 @@
 def main():
-    rope_len = 255
-    original = [x for x in range(0, rope_len+1)]
-    rope = [x for x in range(0, rope_len+1)]
+    rope_len = 256
+    original = [x for x in range(0, rope_len)]
+    rope = [x for x in range(0, rope_len)]
     lengths = [18,1,0,161,255,137,254,252,14,95,165,33,181,168,2,188]
     rope_index = 0
     skip_size = 0
     for length in lengths:
         if length > 1:
-            end_index = (rope_index + length) % rope_len
-            indexes = []
+            end_index = (rope_index + length) % (rope_len)
             if end_index < length:
-                rope_section = (rope[rope_index:] + rope[0:(end_index-1)])[::-1]
-                indexes = original[rope_index:] + original[0:(end_index-1)]
+                section = (rope[rope_index:] + rope[0:end_index])[::-1]
+                indexes = original[rope_index:] + original[0:end_index]
             else:
-                rope_section = rope[rope_index:end_index][::-1]
-                indexes = rope[rope_index:end_index]
+                section = rope[rope_index:end_index][::-1]
+                indexes = original[rope_index:end_index]
+            
+            for i in range(0, length):
+                rope[indexes[i]] = section[i]
 
-            for i in range(0, len(rope_section)):
-                rope[indexes[i]] = rope_section[i]
-
-        rope_index = (rope_index + length + skip_size) % rope_len
+        rope_index = (rope_index + length + skip_size) % (rope_len)
         skip_size += 1
     
     print(rope[0] * rope[1])
