03b7cedc5214d7dfd7469f167b1aaf3d2e285533
Chris Williamson <chris.williamson@bbc.co.uk>
2017-12-14 14:00:51 +0000

    Completed part 2

--- day13/main.py
+++ day13/main.py
@@ -1,20 +1,30 @@
 import math
 
 def is_caught(depth, range):
-    return (depth % (range - 1)) == 0 and math.floor(depth/range) % 2 == 1
+    steps_to_one = (range-1) * 2
+    return depth % steps_to_one == 0
+
+def is_clear_run(depths_to_ranges, delay):
+    for depth, range in depths_to_ranges.items():
+        if is_caught(depth+delay, range):
+            return False
+    return True
 
 def main():
     depths_to_ranges = {}
+
     for line in open('input.txt', 'r').readlines():
         line = line.rstrip().split(': ')
         depths_to_ranges[int(line[0])] = int(line[1])
     
-    severity = 0
-    for depth, range in depths_to_ranges.items():
-        if is_caught(depth, range):
-            severity += (depth * range) 
+    delay = 0
+    clear_run = False
 
-    print(severity)
+    while not clear_run:
+        delay += 1
+        clear_run = is_clear_run(depths_to_ranges, delay)
+
+    print('Clear run is achieved with a delay of %d picoseconds' % delay)
 
 if __name__ == '__main__':
     main()
